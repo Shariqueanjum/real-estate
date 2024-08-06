026d4a0c9266414ca8a021c8b1a573b1a345e5b0
Sharique Anjum <shariqueanjum05@gmail.com>
2024-08-06 15:43:29 +0530

    routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from  './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 const App = () => {
   return (
-    <div>App</div>
+   <BrowserRouter>
+     
+     <Routes>
+          <Route path="/"         element={ <Home />  }   />
+          <Route path="/sign-in"  element={ <SignIn />} />
+          <Route path="/sign-up"  element={ <SignUp />} />
+          <Route path="/profile"  element={ <Profile />}   />
+          <Route path="/about"    element={ <About />  }   />
+
+
+     </Routes>
+   
+   </BrowserRouter>
   )
 }
 
