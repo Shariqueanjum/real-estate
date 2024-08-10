dd047bbe322afcdd5771462d30891159dcae7c29
Sharique Anjum <shariqueanjum05@gmail.com>
2024-08-10 20:23:16 +0530

    Private Route added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import About from  './pages/About'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
@@ -17,9 +18,10 @@ const App = () => {
           <Route path="/"         element={ <Home />  }   />
           <Route path="/sign-in"  element={ <SignIn />} />
           <Route path="/sign-up"  element={ <SignUp />} />
-          <Route path="/profile"  element={ <Profile />}   />
-          <Route path="/about"    element={ <About />  }   />
-
+          <Route path="/about"  element={ <About />}   />
+          <Route element={PrivateRoute}>
+              <Route path="/profile"    element={ <Profile />  }   />
+          </Route>
 
      </Routes>
    
