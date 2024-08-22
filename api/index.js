d6d1fbeabbf01774f3abf588f691fd032b055b70
Sharique Anjum <shariqueanjum05@gmail.com>
2024-08-22 15:41:29 +0530

    added build script and static path

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
 import listingRouter from "./routes/listing.route.js"
+import path from "path";
 
 dotenv.config();
 
@@ -16,6 +17,8 @@ mongoose.connect(process.env.MONGODB_URL)
 .catch((error)=>{
      console.log(error)
 })
+
+const __dirname = path.resolve();
 
 const app=express();
 
@@ -37,6 +40,14 @@ app.use("/api/user" , userRouter);
 app.use("/api/auth" , authRouter);
 app.use("/api/listing" , listingRouter);
 
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+   });
+
+   
 
 app.use((err, req, res, next) => {
      const statusCode = err.statusCode || 500;
