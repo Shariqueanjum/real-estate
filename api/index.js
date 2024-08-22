f2fd4685d779baf441b39dd531b08ca621c32fb9
Sharique Anjum <shariqueanjum05@gmail.com>
2024-08-22 16:22:10 +0530

    Port added

--- api/index.js
+++ api/index.js
@@ -63,8 +63,8 @@ app.use((err, req, res, next) => {
    });
 
 
+   const PORT = process.env.PORT || 3000;
 
-
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
      console.log("Server is running on port 3000");
 })
